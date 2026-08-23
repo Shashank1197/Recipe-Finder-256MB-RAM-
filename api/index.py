f7e7df9371dffbf2c323d64b3dcae23242f1bfcb
Shashank1197 <wwwladdu94@gmail.com>
2026-08-24 00:20:20 +0530

    Update vercel.json to use standard @vercel/python builder on app.py directly and remove api/index.py

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-# Ensure the root directory is in the python path so it can import app.py
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app import app
